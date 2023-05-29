138e401d861de9c5c1a9dda2933087f96f87ab3f
João Victor Reis Alves - JAV <joao.alves@iteris.com.br>
2023-05-29 11:51:57 -0300

    remove test error

--- test/virtual_keyboard_test.dart
+++ test/virtual_keyboard_test.dart
@@ -1,5 +1,5 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:virtual_keyboard/virtual_keyboard.dart';
 
 void main() {
@@ -7,6 +7,8 @@ void main() {
     final keyboard = VirtualKeyboard(
       type: VirtualKeyboardType.Alphanumeric,
       onKeyPress: () => null,
+      backspaceImageBuilder: (context) => Icon(Icons.chevron_left),
+      shadowColor: Colors.grey,
     );
     expect(keyboard.type, VirtualKeyboardType.Alphanumeric);
   });
